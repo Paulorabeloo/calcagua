fc2a246b6c15f42ab0f9d6d00316a9636a081e1b
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-01-22 09:00:22 -0300

    calcagua

---
+++ atv extra - aguadiaria.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int kg, resultado;
+    int ml;
+    ml = 40;
+    kg = 85;
+
+    resultado = ml * kg;
+
+    printf("Você deve beber: %i ml de água por dia", resultado);
+return 0;
+}
